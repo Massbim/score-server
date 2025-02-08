20511550da679721847b4e7619e9899181e209ae
Bim <alamsyahbima22@gmail.com>
2025-02-08 10:49:09 +0700

    fix deployment ip

--- server.js
+++ server.js
@@ -51,8 +51,8 @@ app.get("/", (req, res) => {
 // const IP_ADDRESS = process.env.IP || "localhost";
 const PORT = process.env.PORT;
 server
-  .listen(PORT, IP_ADDRESS, () => {
-    console.log(`Server running on http://127.0.0.1:${PORT}`);
+  .listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
   })
   .on("error", (err) => {
     console.error(`Failed to start server: ${err.message}`);
